60a657be85a90a9062062a0495a38b5d0a148db7
adamTomasiewicz <adam.tomasiewicz@outlook.com>
2019-10-12 16:22:33 +0200

    first commit gifdemo

---
+++ src/main/java/pl/spring/gifdemo/gifdemo/GifdemoApplication.java
@@ -0,0 +1,13 @@
+package pl.spring.gifdemo.gifdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/spring/gifdemo/gifdemo/GifdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.spring.gifdemo.gifdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GifdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
